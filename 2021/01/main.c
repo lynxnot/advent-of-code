05096eb00a2cda14c1fb5c94161c557256f75b83
lynxnot <lynxnot@users.noreply.github.com>
2022-03-22 22:43:00 +0100

    solve AoC 2021 - day 01

---
+++ 2021/01/main.c
@@ -0,0 +1,66 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+
+#if 0
+const char *INFILE = "input.test";
+#define MAX_MEASUREMENTS 10
+#else
+const char *INFILE = "input";
+#define MAX_MEASUREMENTS 2000
+#endif
+
+int depths[MAX_MEASUREMENTS] = {0};
+
+void parseMeasurements(const char *path)
+{
+    FILE *fp;
+    char *line = NULL;
+    size_t len = 0;
+
+    fp = fopen(INFILE, "r");
+    if (fp == NULL)
+    {
+        perror("uWu made a BuBu");
+        exit(EXIT_FAILURE);
+    }
+
+    int i = 0;
+    while (getline(&line, &len, fp) != -1)
+    {
+        depths[i] = atoi(line);
+        i++;
+    }
+
+    free(line);
+    fclose(fp);
+}
+
+int main()
+{
+    printf("** AoC 2021 - Day 01\n");
+
+    parseMeasurements(INFILE);
+
+    // part 1
+    int increased = 0;
+    for (int i = 0; i < MAX_MEASUREMENTS - 1; i++)
+        if (depths[i + 1] > depths[i])
+            increased++;
+
+    printf("Depth has increased %d times.\n", increased);
+
+    // part 2
+    int sums[MAX_MEASUREMENTS - 2] = {0};
+    for (int i = 0; i < MAX_MEASUREMENTS - 2; i++)
+        sums[i] = depths[i] + depths[i + 1] + depths[i + 2];
+
+    int trincreased = 0;
+    for (int i = 0; i < MAX_MEASUREMENTS - 3; i++)
+        if (sums[i + 1] > sums[i])
+            trincreased++;
+
+    printf("Tri-Depth has increased %d times.\n", trincreased);
+
+    exit(EXIT_SUCCESS);
+}
